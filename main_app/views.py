2fce150170025e2b7eb6a4669dd62ea3f513dcce
orache arthur <arthur.orache@gmail.com>
2018-05-21 09:55:11 +0300

    ch(urls): Level Two- Templates and Models
    - Add templates
    - Add static files
    - Add models
    - Interact with models

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Treasure
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1>Welcome Home Tracker!</h1>')
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures': treasures})
+
+
+# class Treasure:
+#     def __init__(self, name, value, material, location, img_url):
+#         self.name = name
+#         self.value = value
+#         self.material = value
+#         self.location = location
+#         self.img_url = img_url
+
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM", 'example.com/nugget.jpg'),
+    Treasure('Fools Gold', 0.00, 'pyrite', "Fool's Falls, CO", 'example.com/fools-gold.jpg'),
+    Treasure('Coffee Can', 20.00, 'tin', "Acme, CA", 'example.com/coffee-can.jpg'),
+]
