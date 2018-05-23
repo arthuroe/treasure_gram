5ce93ed21284fee17640b15546011848de3115ac
orache arthur <arthur.orache@gmail.com>
2018-05-23 20:18:58 +0300

    Part 2-level-4(user,authentication):
    - Link treasure to users through user model
    - Add user profile page
    - Add authentication
    - Add authentication links
    - Add required templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^user/(\w+)', views.profile, name='profile'),
     url(r'^([0-9]+)/$', views.detail, name='detail'),
     url(r'^post_url/$', views.post_treasure, name='post_treasure'),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
 ]
 
 # Add to the bottom of your file
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .models import Treasure
-from .forms import TreasureForm
+from .forms import TreasureForm, LoginForm
 # Create your views here.
 
 
@@ -19,5 +21,43 @@ def detail(request, treasure_id):
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save(commit=True)
+        treasure = form.save(commit=False)
+        treasure.user = request.user
+        treasure.save()
+    return HttpResponseRedirect('/')
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request, 'profile.html', {'treasures': treasures, 'username': username})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print('The Account was deactivated')
+
+            else:
+                print('The username and password are incorrect')
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def registration(request):
+    pass
+
+
+def logout_view(request):
+    logout(request)
     return HttpResponseRedirect('/')
